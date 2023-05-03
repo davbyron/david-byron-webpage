abc7bbcac021c9db452b4a35eac4013c921a0d4c
David Byron <davbyron@icloud.com>
2023-05-03 09:49:21 -0500

    add Google fonts to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Dosis&family=Roboto:ital,wght@0,300;0,400;1,300;1,400&display=swap" rel="stylesheet" />
+      </head>
       <body>{children}</body>
     </html>
   );
