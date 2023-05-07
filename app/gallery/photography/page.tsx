2f375773df10ed1586b5197d6855b471a51d86cb
David Byron <davbyron@icloud.com>
2023-05-06 21:42:40 -0500

    add photography gallery

--- app/gallery/photography/page.tsx
+++ app/gallery/photography/page.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import Nav from '../../../components/Nav'
+import PhotoGallery from '../../../components/PhotoGallery'
 
 function Photography() {
   return (
-    <Nav activePath='/gallery/photography' />
+    <>
+      <Nav activePath='/gallery/photography' />
+      <main>
+        <section id="gallery">
+          <h2>GALLERY</h2>
+          <h3>PHOTOGRAPHY</h3>
+          <PhotoGallery category='photography' />
+        </section>
+      </main>
+    </>
   )
 }
 
